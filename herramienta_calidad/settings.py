0b53bb30811f3f7dd44313ba20756e7fdd848ffa
SantiagoCasanas <santiago01.02.003@gmail.com>
2024-08-19 23:17:45 -0500

    despliegue

--- herramienta_calidad/settings.py
+++ herramienta_calidad/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'herramienta_calidad.urls'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
